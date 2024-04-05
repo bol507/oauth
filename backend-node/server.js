10905338a84df6a5e57ca8b50fea420e4e6fbcce
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-04-05 08:42:18 -0500

    feat: add frontend url to env file

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const axios = require('axios')
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const axios = require("axios");
 
-dotenv.config({ path: `.env.local`});
+dotenv.config({ path: `.env.local` });
 
-const port = process.env.PORT
-const clientId = process.env.CLIENT_ID
-const clientSecret = process.env.CLIENT_SECRET
-const githubUrl = process.env.GITHUB_URL
-const redirectUri = '/oauth/redirect'
-//const refreshToken = process.env.REFRESH_TOKEN
-const url = `${githubUrl}?client_id=${clientId}&client_secret=${clientSecret}&code=`
+const port = process.env.PORT;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const githubUrl = process.env.GITHUB_URL;
+const redirectUri = "/oauth/redirect";
+const frontendUrl = process.env.FRONTEND_URL;
+const url = `${githubUrl}?client_id=${clientId}&client_secret=${clientSecret}&code=`;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors({credentials:true, origin:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: true }));
 
-app.get('/',(req,res) => res.send('Express Server'));
-app.get('/oauth/redirect', async (req,res) => {
-    await axios({
-        method:"POST",
-        url: url+ `${req.query.code}`,
-        headers:{
-            Accept: "application/json",
-        }
-    }).then((response)=>{
-        res.redirect(`http://bdelgado.duckdns.org?access_token=${response.data.access_token}`);
-    });
+app.get("/", (req, res) => res.send("Express Server"));
+app.get("/oauth/redirect", async (req, res) => {
+  await axios({
+    method: "POST",
+    url: url + `${req.query.code}`,
+    headers: {
+      Accept: "application/json",
+    },
+  }).then((response) => {
+    res.redirect(
+      `${frontendUrl}?access_token=${response.data.access_token}`
+    );
+  });
 });
 
-app.listen(port, () => 
-    console.log(`listening on port ${port}`)
-)
+app.listen(port, () => console.log(`listening on port ${port}`));
