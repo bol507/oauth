3cd175386ab0da78e6bc142fc3f5dfbf29577a4d
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-04-05 18:24:08 -0500

    refactor: index.ts

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -1,37 +1,14 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const axios = require("axios");
-import { Request, Response } from 'express';
-import { AxiosResponse } from 'axios';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config({ path: `.env.local` });
-
+const app = express();
 const port = process.env.PORT;
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const githubUrl = process.env.GITHUB_URL;
-const frontendUrl = process.env.FRONTEND_URL;
-const url = `${githubUrl}?client_id=${clientId}&client_secret=${clientSecret}&code=`;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: true }));
-
-app.get("/oauth/redirect", async (req: Request, res: Response) => {
-  await axios({
-    method: "POST",
-    url: url + `${req.query.code}`,
-    headers: {
-      Accept: "application/json",
-    },
-  }).then((response: AxiosResponse<{ access_token: string }>) => {
-    res.redirect(
-      `${frontendUrl}?access_token=${response.data.access_token}`
-    );
-  });
-});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
---
+++ backend-node/src/routes/oauth.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import axios, { AxiosResponse } from 'axios';
+import dotenv from 'dotenv';
+
+const router = express.Router();
+dotenv.config({ path: `.env.local` });
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const githubUrl = process.env.GITHUB_URL;
+const frontendUrl = process.env.FRONTEND_URL;
+const url = `${githubUrl}?client_id=${clientId}&client_secret=${clientSecret}&code=`;
+
+router.get('/oauth/redirect', async (req: Request, res: Response) => {
+  await axios({
+    method: 'POST',
+    url: url + `${req.query.code}`,
+    headers: {
+      Accept: 'application/json'
+    }
+  }).then((response: AxiosResponse<{ access_token: string }>) => {
+    res.redirect(`${frontendUrl}?access_token=${response.data.access_token}`);
+  });
+});
+
+export default router;
